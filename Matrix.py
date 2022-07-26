4bc0d46c03a37bbc9d5b292313b335928b242371
ValeevRauf <valeev.RR@phystech.edu>
2022-07-26 17:05:45 +0300

    create master

--- Matrix.py
+++ Matrix.py
@@ -2,23 +2,42 @@ import numpy as np
 
 
 class Matrix:
-    def __init__(self) -> None:
-        print('Please, enter matrix sizes in format "n m"')
-        (self.n, self.m) = (int(i) for i in input().split(' '))
-        print('Please, enter matrix')
-        matrix = []
-        for row_number in range(self.n):
-            s = input()
-            matrix.append([int(i) for i in s.split(' ')])
-        self.matrix = np.array(matrix)
+    def __init__(self, n: int, m: int) -> None:
+        self.n = n
+        self.m = m
+        self.matrix = np.array([])
 
-    def print_variables(self) -> None:
-        print(self.n, self.m)
-        print(self.matrix)
+    def matrix_init(self, console=False, input_matrix=None) -> None:
+        if console:
+            print('Enter matrix')
+            self.matrix = np.array([])
+            for i in range(self.n):
+                line = [int(element) for element in input().split(' ')]
+                np.append(self.matrix, line)
+        else:
+            self.matrix = input_matrix
 
-    def rotate(self) -> np.array:
+    def rotate(self) -> None:
         result = np.zeros((self.m, self.n))
         for row_number in range(self.matrix.shape[1]):
             for column_number in range(self.matrix.shape[0]):
                 result[row_number, column_number] = self.matrix[column_number, self.m - row_number - 1]
         self.matrix = result
+
+    def transpose(self, inplace=False):
+        result = np.zeros((self.m, self.n))
+
+        for row_number in range(self.matrix.shape[1]):
+            for column_number in range(self.matrix.shape[0]):
+                result[row_number, column_number] = self.matrix[self.n - column_number - 1, self.m - row_number - 1]
+
+        if inplace:
+            self.matrix = result
+            self.m, self.n = self.n, self.m
+        else:
+            return result
+
+
+M = Matrix(int(input()), int(input()))
+M.matrix_init(console=True)
+print(M.transpose(inplace=False))
